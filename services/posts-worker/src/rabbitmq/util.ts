81f99d710fd034147282f2bdd0c75bdc7dda8dc5
Subhan <s.ashraf9@protonmail.com>
2022-03-06 18:07:27 +0000

    move redis utils

--- services/posts-worker/src/rabbitmq/util.ts
+++ services/posts-worker/src/rabbitmq/util.ts
@@ -1,8 +1,8 @@
-import redis, { RedisKeys } from "../redis/client";
+import { addToFeeds, updateFeeds, deleteFromFeeds } from "../redis/util";
 import { getFollowers } from "../grpc/followsClient";
 
 type Action = "post" | "update" | "delete";
-interface Post {
+export interface Post {
     id: number;
     userid: number;
     post: string;
@@ -41,77 +41,5 @@ export const processMessages = async (received: any) => {
             break;
         default:
             break;
-    }
-};
-
-export const addToFeeds = async (
-    poststring: string,
-    post: Post,
-    followers: any[]
-) => {
-    try {
-        await redis.ZADD(RedisKeys.UserPosts + post.userid, {
-            score: parseInt(post.timestamp),
-            value: poststring,
-        });
-
-        for (let i = 0; i < followers.length; i++) {
-            await redis.ZADD(RedisKeys.Timeline + followers[i].followerid, {
-                score: parseInt(post.timestamp),
-                value: poststring,
-            });
-        }
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-export const updateFeeds = async (
-    oldpost: string,
-    newpost: string,
-    post: Post,
-    followers: any[]
-) => {
-    try {
-        redis
-            .multi()
-            .ZREM(RedisKeys.UserPosts + post.userid, oldpost)
-            .ZADD(RedisKeys.UserPosts + post.userid, {
-                score: parseInt(post.timestamp),
-                value: newpost,
-            })
-            .exec();
-
-        for (let i = 0; i < followers.length; i++) {
-            redis
-                .multi()
-                .ZREM(RedisKeys.Timeline + followers[i].followerid, oldpost)
-                .ZADD(RedisKeys.Timeline + followers[i].followerid, {
-                    score: parseInt(post.timestamp),
-                    value: newpost,
-                })
-                .exec();
-        }
-    } catch (err) {
-        console.log(err);
-    }
-};
-
-export const deleteFromFeeds = async (
-    oldpost: string,
-    post: Post,
-    followers: any[]
-) => {
-    try {
-        await redis.ZREM(RedisKeys.UserPosts + post.userid, oldpost);
-
-        for (let i = 0; i < followers.length; i++) {
-            await redis.ZREM(
-                RedisKeys.Timeline + followers[i].followerid,
-                oldpost
-            );
-        }
-    } catch (err) {
-        console.log(err);
     }
 };
